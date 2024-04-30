754013a802eb6daf84bf96004f5b25840c57a680
HuseyinK0r0glu <koroglu27@gmail.com>
2024-04-30 19:48:58 +0300

    version6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Starting TicTacToe game");
+        System.out.println("May the best man win");
         TicTacToe tictactoe = new TicTacToe();
     }
 }
